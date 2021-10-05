abd27804bc5179db25b18cd10f4aca39f7621831
kjimlau <neochannel@163.com>
2021-10-05 10:05:58 +0800

    chore: initialize

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+export function activate(context: vscode.ExtensionContext) {
+  console.log('active');
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
