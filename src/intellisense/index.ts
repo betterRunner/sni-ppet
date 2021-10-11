3a0c61d0a4ea50a52ed66442704c45dd484b8ddb
kjimlau <neochannel@163.com>
2021-10-11 14:15:15 +0800

    feat: intellisense

---
+++ src/intellisense/index.ts
@@ -0,0 +1,81 @@
+import * as vscode from "vscode";
+import { readMetasFromConfig } from "../metas";
+import { IntellisenseMeta } from "../types/meta";
+import { getPositionByOffset } from "../utils/vscode";
+import { Snippet } from "../types/snippet";
+import { genCompletionItem, genCompletionItemsByMeta } from "./utils";
+
+export async function getCompletionItemsByContextText(
+  contextText: string
+): Promise<vscode.CompletionItem[]> {
+  const _genCompletionItems = (
+    meta: IntellisenseMeta
+  ): vscode.CompletionItem[] => {
+    const { snippetMetas = [], genSnippetFn, effects = [] } = meta;
+
+    // only meta with non-empty `itemsMap` need progressive command
+    const progressiveMatchTags = Array.from(
+      new Set(
+        snippetMetas.reduce(
+          (prev, cur) =>
+            Object.keys(cur.itemsMap).length ? [...prev, cur.matchTag] : prev,
+          [] as string[]
+        )
+      )
+    );
+    const reEnter = new RegExp(
+      `(?:^|\\s+)(${progressiveMatchTags.join("|")}).$`
+    );
+    const match = contextText.match(reEnter);
+    if (match) {
+      // matching case `.` is the time to record the position
+      const startPosition = getPositionByOffset(0, match[1].length + 1);
+
+      // generate completion items from snippetMeta
+      return snippetMetas.reduce(
+        (prev, snippetMeta) => [
+          ...prev,
+          ...genCompletionItemsByMeta(
+            snippetMeta,
+            startPosition,
+            genSnippetFn,
+            effects
+          ),
+        ],
+        [] as vscode.CompletionItem[]
+      );
+    }
+
+    // default completion item
+    const matchTags = Array.from(
+      new Set(
+        snippetMetas.reduce(
+          (prev, cur) => [...prev, cur.matchTag],
+          [] as string[]
+        )
+      )
+    );
+    return matchTags.map((t) => {
+      const isProgressive = progressiveMatchTags.includes(t);
+      let snippet;
+      if (!isProgressive) {
+        // non-progressive meta needs to give the snippet parameter to `genCompletionItem` thus generating the snippet directly
+        const snippetMeta = snippetMetas.find((meta) => meta.matchTag === t);
+        snippet = {
+          tpl: snippetMeta?.tpl || "",
+          variables: {},
+          genSnippetFn: (_) => _,
+          effectPatches: meta.effects,
+        } as Snippet;
+      }
+      const startPosition = getPositionByOffset(0, 1);
+      const item = genCompletionItem(t, startPosition, snippet);
+      return item;
+    });
+  };
+  const metas = await readMetasFromConfig();
+  return metas.reduce(
+    (prev, meta) => [...prev, ..._genCompletionItems(meta)],
+    [] as vscode.CompletionItem[]
+  );
+}
---
+++ src/intellisense/utils.ts
@@ -0,0 +1,93 @@
+import {
+  Command,
+  QuickPickItem,
+  CompletionItemKind,
+  CompletionItem,
+  Position,
+} from "vscode";
+import { SnippetMeta, MetaOption } from "../types/meta";
+import { IntellisenseCommandArguments } from "../types/intellisense";
+import { Snippet } from "../types/snippet";
+import { GenSnippetFn, Patch } from "../types/common";
+import { fillMetaVariables } from "../metas/utils";
+
+function genCommandOptions(
+  options: Record<string, MetaOption>
+): QuickPickItem[] {
+  const keys = Object.keys(options);
+  return keys.map((key) => ({
+    label: key,
+    picked: options[key]?.picked,
+    value: options[key]?.value,
+  }));
+}
+
+export function genCompletionItem(
+  label: string,
+  startPosition: Position | undefined,
+  snippet?: Snippet,
+  options?: Record<string, MetaOption> | undefined,
+  detail: string = "",
+  documentation: string = ""
+): CompletionItem {
+  // (1) if `snippet` is null, does not create `command`
+  // (2) if `snippet` is not null and `options` is null, create `insertSnippet` command
+  // (3) if `snippet` and `options` are not null, create `showQuickPick` command (which will later call `insertSnippet` command)
+  const command: Command | undefined = snippet
+    ? {
+        title: "options",
+        command: options ? "showQuickPick" : "insertSnippet",
+        arguments: [
+          {
+            options: options && genCommandOptions(options),
+            snippet,
+            startPosition
+          } as IntellisenseCommandArguments,
+        ],
+      }
+    : undefined;
+
+  return {
+    label,
+    detail,
+    documentation,
+    command,
+    preselect: true,
+    kind: CompletionItemKind.Value,
+  };
+}
+
+export function genCompletionItemsByMeta(
+  meta: SnippetMeta,
+  startPosition: Position | undefined,
+  genSnippetFn: GenSnippetFn,
+  generalEffects: Patch[]
+): CompletionItem[] {
+  const { tpl, commonValues, itemsMap = {}, effects: metaEffects = [] } = meta;
+  const keys = Object.keys(itemsMap);
+  return keys.map((key) => {
+    const {
+      detail,
+      documentation,
+      variables,
+      options,
+      effects = [],
+    } = itemsMap[key];
+    // snippet
+    const snippet: Snippet = {
+      tpl: tpl,
+      variables: fillMetaVariables(commonValues, variables),
+      genSnippetFn,
+      effectPatches: [...generalEffects, ...metaEffects, ...effects],
+    };
+
+    return genCompletionItem(
+      key,
+      startPosition,
+      snippet,
+      options,
+      detail,
+      documentation
+    );
+  });
+}
